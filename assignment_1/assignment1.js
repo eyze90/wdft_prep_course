a01db9491b605fa6e574ecea43e9cd9439374a99
eyze90 <yu.eugene90@gmail.com>
2017-10-07 17:51:24 +0800

    Assignment 2 Completed

--- assignment_1/assignment1.js
+++ assignment_1/assignment1.js
@@ -5,6 +5,11 @@ Create a variable named "numbers" and assign an empty array to it.
 var numbers;
 console.log('Hello World');
 numbers = [];
+console.log(numbers);
+
+//Hands-on Challenge
+var name = 'Eugene';
+console.log('Hello ' + name);
 
 
 /*
@@ -14,16 +19,28 @@ Using the loop provided for you below, use the array push method to insert the n
 
 for(var i=0;i<10;i++){
     //use the array push() method here.
+    numbers.push(i+1);
 }
+console.log(numbers);
+
+//Hands-on Challenge
+var x = 1125;
+x %= 2;
+console.log(x);
 
 
 /*
 Exercise #3
 Test that you used the array push method correctly by console logging the first item in the array. Console log the last item in the array afterwards.
 */
-console.log();
-console.log();
+console.log(numbers[0]);
+console.log(numbers[9]);
 
+//Hands-on Challenge
+var a = true;
+var b = false;
+var c = true;
+console.log(a || b || c);
 
 /*
 Exercise #5
@@ -31,5 +48,35 @@ Create an empty object and assign it to the variable car below.
 Give the car object a property called "colour" and assign it the value of "black". Test that your code works correctly by outputting the "colour" property to the console. You should see the value "black" printed out.
 */
 var car;
+car = {colour:'black'};
+console.log(car.colour);
+console.log(car[0]);
+
+//Hands-on Challenge
+var answer1 = 10 - 3 * 5 / 4 + 202 % 3;
+var answer2 = 10 - (3 * (5/4) + (202 % 3));
+console.log('answer1: ' + answer1);
+console.log('answer2: ' + answer2);
+
+//Exercise #6
+//Hands-on Challenge
+x = '12';
+console.log(x.toString());
+console.log(parseInt('113'));
+console.log(parseInt('1.12356'));
+x = parseInt('Hello World');
+console.log(x);
+
+//Diving Deeper
+//Hands-on Challenge
+var match = 'BrainStation';
+var string1 = "I am a BrainStation student.";
+var string2 = "I am a student.";
+
+console.log(string1.search(match));
+console.log(string2.search(match));
+
+var newstring1 = string1.replace("BrainStation student", "Junior Developer");
+console.log(newstring1);
 
 
--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -1,21 +1,69 @@
 // Exercise #1
 // Write your solution below
-
+function printTriangle(char,totalRows){
+    for (var row=1;row<=totalRows;row++) {
+        var printout = char;        
+        for(var y=1;y<row;y++){
+            printout += char;
+        }
+        console.log(printout);
+    }
+}
+printTriangle('#',7);
+printTriangle('$',15);
 
 /* 
 Write your explanation for Exercise #1 here
-
+Function that takes two parameters, a character/string and an int.
+c: character or string to be printed in each row.
+totalRows: number of rows of the triangle.
+1st row will print the character once, 2nd row twice and so on.
 */
 
-
-
-
+//Hands-on Challenge
+for(var i=1;i<=100;i++) {
+    if(i % 5 == 0 && i % 3 == 0)
+        console.log("fizzbuzz");
+    else
+        if(i % 5 == 0)
+            console.log("fizz");
+        else if(i % 3 == 0)
+            console.log("buzz");
+        else
+            console.log(i);
+}
 
 // Exercise #2
 // Write your solution below
+function isEven(number) {
+    if(number % 2 == 0)
+        console.log('the number is even!');
+    else
+        console.log('the number is odd!')
+}
 
+isEven(7);
+isEven(4);
 
 /* 
 Write your explanation for Exercise #2 here
-
+Modulo operator returns the remainder of the division.
+Even numbers have a remainder of 0 when divided by 2.
+Odd numbers have a remainder of 1 when diveded by 2.
 */
+
+//Hands-on Challenge
+function printGrid(char1,char2,maxRows) {
+    for(var x=1;x<=maxRows;x++) { //x = row
+        var printout = '';
+        for(var y=1;y<=maxRows;y++){ //y = column
+            if((x%2 != 0 && y%2 != 0) || (x%2 == 0 && y%2 == 0))
+                printout += char1;
+            else   
+                printout += char2;
+        }
+        console.log(printout);
+    }        
+}
+printGrid('#',' ',8);
+printGrid('X','O',10);
